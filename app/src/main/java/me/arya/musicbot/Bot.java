310692751b9619062fdf0ecc4c6d68dc81e3ec30
Arya K. O <67804463+Arya-Programmer@users.noreply.github.com>
2021-09-20 15:45:34 +0300

    Adding Shutdown command

--- app/src/main/java/me/arya/musicbot/Bot.java
+++ app/src/main/java/me/arya/musicbot/Bot.java
@@ -1,16 +1,20 @@
 package me.arya.musicbot;
 
 import net.dv8tion.jda.api.JDABuilder;
+import net.dv8tion.jda.api.entities.Activity;
+
+import javax.security.auth.login.LoginException;
 
 public class Bot {
-    private Bot() {
+    private Bot() throws LoginException {
         new JDABuilder()
-            .setToken("ODg5MjQxNjgyMTM3MTk4NjYy.YUeY2g.0rpIej0TtcLnjGRGSDnDUy6ZV_k")
-            .addEventListener(new Listener())
+            .setToken(Config.get("TOKEN"))
+            .addEventListeners(new Listener())
+            .setActivity(Activity.watching("."))
             .build();
     }
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws LoginException {
         new Bot();
     }
 }
--- app/src/main/java/me/arya/musicbot/Listener.java
+++ app/src/main/java/me/arya/musicbot/Listener.java
@@ -1,15 +1,35 @@
 package me.arya.musicbot;
 
 
+import me.duncte123.botcommons.BotCommons;
 import net.dv8tion.jda.api.events.ReadyEvent;
+import net.dv8tion.jda.api.events.message.guild.GuildMessageReceivedEvent;
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import javax.annotation.Nonnull;
 
 
 public class Listener extends ListenerAdapter {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(Listener.class);
+
     @Override
     public void onReady(@Nonnull ReadyEvent event) {
-        System.out.printf("%#s is ready", event.getJDA().getSelfUser());
+        LOGGER.info("{} is ready", event.getJDA().getSelfUser().getAsTag());
+    }
+
+    @Override
+    public void onGuildMessageReceived(@Nonnull GuildMessageReceivedEvent event) {
+        String prefix = Config.get("prefix");
+        String raw = event.getMessage().getContentRaw();
+
+        if (raw.equalsIgnoreCase(prefix+"shutdown")
+                && event.getAuthor().getId().equals(Config.get("owner_id"))) {
+            LOGGER.info("Shutting Down");
+            event.getJDA().shutdown();
+            BotCommons.shutdown(event.getJDA());
+        }
     }
 }
