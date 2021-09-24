e4f6b38491c1fb7963e3c17f4cfe0af3f7cff6a8
Arya K. O <67804463+Arya-Programmer@users.noreply.github.com>
2021-09-24 22:21:37 +0300

    Move checking for dotenv values in Constructor

--- app/src/main/java/me/arya/musicbot/Config.java
+++ app/src/main/java/me/arya/musicbot/Config.java
@@ -1,17 +1,21 @@
 package me.arya.musicbot;
 
+import io.github.cdimascio.dotenv.DotEnvException;
 import io.github.cdimascio.dotenv.Dotenv;
 
 public class Config {
 
-    private static final Dotenv dotenv = Dotenv.load();
+    private static Object dotenv;
+
+    public Config() {
+        try {
+            dotenv = Dotenv.load();
+        } catch DotEnvException e {
+            dotenv = System.getenv();
+        }
+    }
 
     public static String get(String key) {
-        key = key.toUpperCase();
-        try {
-            return dotenv.get(key);
-        } catch (ExceptionInInitializerError e) {
-            return System.getenv().get(key);
-        }
+        return dotenv.get(key.toUpperCase());
     }
 }
