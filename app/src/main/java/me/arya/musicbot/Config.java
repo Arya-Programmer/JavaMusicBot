b0a796e02ed28474c82e85251de1ef5172d12812
Arya K. O <67804463+Arya-Programmer@users.noreply.github.com>
2021-09-24 21:56:49 +0300

    Check for which values should be used

--- app/src/main/java/me/arya/musicbot/Config.java
+++ app/src/main/java/me/arya/musicbot/Config.java
@@ -1,13 +1,16 @@
 package me.arya.musicbot;
 
-// Uncomment this to run app locally
-// import io.github.cdimascio.dotenv.Dotenv;
+import io.github.cdimascio.dotenv.Dotenv;
 
 public class Config {
 
-//    private static final Dotenv dotenv = Dotenv.load();
+    private static final Dotenv dotenv = Dotenv.load();
 
     public static String get(String key) {
-        return System.getenv().get(key.toUpperCase());
+        key = key.toUpperCase();
+        if (System.getenv().get(key) == null) {
+            return dotenv.get(key);
+        }
+        return System.getenv().get(key);
     }
 }
