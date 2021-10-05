8f419e68e92305793cf041e4208f0e17d86536db
Arya K. O <67804463+Arya-Programmer@users.noreply.github.com>
2021-10-05 17:53:22 +0300

    Fix loading playlist from link, make queue load five songs after and before current song

--- app/src/main/java/me/arya/musicbot/command/commands/music/QueueCommand.java
+++ app/src/main/java/me/arya/musicbot/command/commands/music/QueueCommand.java
@@ -8,6 +8,8 @@ import me.arya.musicbot.lavaplayer.GuildMusicManager;
 import me.arya.musicbot.lavaplayer.PlayerManager;
 import net.dv8tion.jda.api.entities.TextChannel;
 import net.dv8tion.jda.api.requests.restaction.MessageAction;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -15,6 +17,7 @@ import java.util.concurrent.BlockingQueue;
 import java.util.concurrent.TimeUnit;
 
 public class QueueCommand implements ICommand {
+    private static final Logger LOGGER = LoggerFactory.getLogger(QueueCommand.class);
     @SuppressWarnings("ResultOfMethodCallIgnored")
     @Override
     public void handle(CommandContext ctx) {
@@ -27,11 +30,18 @@ public class QueueCommand implements ICommand {
             return;
         }
 
-        final int trackCount = Math.min(queue.size(), 10);
         final List<AudioTrack> trackList = new ArrayList<>(queue);
+
+        final AudioTrack currentTrack = musicManager.audioPlayer.getPlayingTrack();
+        final int indexCurrentTrack = trackList.indexOf(currentTrack);
+
+        final int tracksNumBeforeCurrent = Math.max(indexCurrentTrack-5, 0);
+        final int tracksNumAfterCurrent = Math.min(indexCurrentTrack+5, queue.size());
         final MessageAction messageAction = channel.sendMessage("**Current Queue:**\n").append("```haskell\n");
 
-        for (int i=0; i < trackCount; i++) {
+        LOGGER.info(currentTrack.getInfo().toString());
+
+        for (int i=tracksNumBeforeCurrent; i < tracksNumAfterCurrent; i++) {
             final AudioTrack track = trackList.get(i);
             final AudioTrackInfo info = track.getInfo();
 
@@ -40,7 +50,7 @@ public class QueueCommand implements ICommand {
 
             if (info.title.length() < 40) {
                 messageAction.append(info.title);
-                for (int j=0; j < (40-info.title.length()); j++) messageAction.append(" ");
+                for (int j=0; j < (42-info.title.length()); j++) messageAction.append(" ");
             } else {
                 messageAction.append(info.title.substring(0, 39))
                         .append("   ");
@@ -49,10 +59,12 @@ public class QueueCommand implements ICommand {
                     .append("\n");
         }
 
-        if (trackList.size() > trackCount) {
-            messageAction.append("\n    ")
-                    .append(String.valueOf(trackList.size() - trackCount))
+        messageAction.append("\n    ");
+        if (trackList.size() > tracksNumAfterCurrent) {
+            messageAction.append(String.valueOf(trackList.size() - tracksNumAfterCurrent))
                     .append(" more track(s)");
+        } else {
+            messageAction.append("   This is the end of the queue!");
         }
 
         messageAction.append("```").queue();
