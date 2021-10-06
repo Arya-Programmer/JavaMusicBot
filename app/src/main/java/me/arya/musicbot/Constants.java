d2acb522c00917ef9dfeae7c2fc4504790fa0bcd
Arya K. O <67804463+Arya-Programmer@users.noreply.github.com>
2021-10-06 12:57:34 +0300

    Retrive prefixes from database and insert if not exists

---
+++ app/src/main/java/me/arya/musicbot/Constants.java
@@ -0,0 +1,10 @@
+package me.arya.musicbot;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Constants {
+
+    public static final Map<Long, String> PREFIXES = new HashMap<>();
+
+}
